0fe4973bfdbbddb7bad892d75552f1c8043e90bb
pppa2019 <1425313146@qq.com>
2022-10-07 10:44:13 +0800

    revised data processing

--- data_process.py
+++ data_process.py
@@ -1,35 +1,62 @@
 import json
 import re
+from typing import DefaultDict
+
 answer_path = 'situation-data/answers.txt'
-question_id_mapper = [r'[1-2].[0-9a-z]{1,3}\.', r'[1-2].[0-9a-z]{1,3}\...Variant:']
-answer_id_mapper = [r'[1-2].[0-9a-z]{1,3}.answer:', r'[1-2].[0-9a-z]{1,3}.variant answer:']
-source_pattern = r'\(\)'
-qa_pair = {}
+question_id_mapper = [r'[1-2].[0-9a-z]{1,3}\.\s\s', r'[1-2].[0-9a-z]{1,3}\.\s\sVariant:\s']
+answer_id_mapper = [r'[1-2].[0-9a-z]{1,3}.answer:\s\s', r'[1-2].[0-9a-z]{1,3}\svariant answer:\s\s']
+pure_id_pattern = r'[1-2].[0-9]{1,3}'
+source_pattern = r'\(.{0,30}\)'
+answer_source_pattern = r'\([fF]rom.{0,30}\)'
+def dict_template():
+    return {
+        "senario": [],
+        "answer": [],
+        "question": "How could this be?"
+    }
+qa_pair = DefaultDict(dict_template)
 with open(answer_path, 'r') as f:
     lines = f.readlines()
     line_id = 0
     lines_count = len(lines)
     text = []
-    question_mode = True
+    id = None
     while line_id < lines_count:
-
         line = lines[line_id].strip()
+        full_text = None
         if len(line)>0:
             text.append(line)
+                
         else:
+            full_text = ' '.join(text)
+            for pattern in question_id_mapper:
+                map_result = re.match(pattern, full_text)
+                if map_result:
+                    id = map_result.group()
+                    question_mode = True
+            for pattern in answer_id_mapper:
+                map_result = re.match(pattern, full_text)
+                if map_result:
+                    id = map_result.group()
+                    question_mode = False
+            pure_id = re.match(pure_id_pattern, id).group()
+            if pure_id=='2.34':
+                import ipdb;ipdb.set_trace()
             if question_mode:
-                question = ' '.join(text)
+                question = full_text.split(id)[-1]
+                question = re.sub(source_pattern, '', question)
                 text = []
-                question_mode = False
+                qa_pair[pure_id]['senario'].append(question)
             elif not question_mode:
-                answer = ' '.join(text)
+                answer = full_text.split(id)[-1]
+                answer = re.sub(answer_source_pattern, '', answer)
                 text = []
-                qa_pair[question] = answer
-                question_mode = True
+                qa_pair[pure_id]['answer'].append(answer)
+                # qa_pair[question] = answer
                 # import ipdb;ipdb.set_trace()
+            full_text = None
             while(len(lines[line_id+1].strip())==0):
                 line_id += 1
         line_id += 1
     with open('puzzles.json', 'w') as f:
         json.dump(qa_pair, f)
-    import ipdb;ipdb.set_trace()
